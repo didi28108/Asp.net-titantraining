13410d8a1fe3182b1d31842b7ce7971fa871f463
Alex Wang Chong-Yu <alex.wang@titansoft.com.sg>
2019-04-25 02:26:07 +0800

    set up project and add CRUD function

---
+++ MemberManagementSystem/Controllers/MemberController.cs
@@ -0,0 +1,45 @@
+﻿using MemberManagementSystem.Service;
+using MemberManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberManagementSystem.Controllers
+{
+    public class MemberController : Controller
+    {
+        MemberService memberService = new MemberService();
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View(memberService.GetAllMembers());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Member member)
+        {
+            memberService.CreateMember(member);
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            memberService.DeleteMember(id);
+            return RedirectToAction("index");
+        }
+        public ActionResult Edit(int id)
+        {
+            return PartialView(memberService.GetMemberById(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Member member)
+        {
+            memberService.EditMember(member);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ MemberManagementSystem/Service/MemberService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MemberManagementSystem.Models;
+
+namespace MemberManagementSystem.Service
+{
+    public class MemberService
+    {
+        readonly Models.MemberEntities db = new Models.MemberEntities();
+
+        public List<Member> GetAllMembers()
+        {
+            return db.Member.ToList();
+        }
+        public Member GetMemberById(int id)
+        {
+            return db.Member.Find(id);
+        }
+        public void CreateMember(Member member)
+        {
+            db.Member.Add(member);
+            db.SaveChanges();
+        }
+        public void DeleteMember(int id)
+        {
+            Member target = db.Member.Find(id);
+            db.Member.Remove(target);
+            db.SaveChanges();
+        }
+        public void EditMember(Member member)
+        {
+            Member target = GetMemberById(member.Id);
+            db.Entry(target).CurrentValues.SetValues(member);
+            db.SaveChanges();
+        }
+    }
+}
